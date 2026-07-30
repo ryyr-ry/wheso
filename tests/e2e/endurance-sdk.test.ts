4f3b3dda30ba58a6beb62e5ee4be4e942d39bcfa
ryyr_ry <git-commit@ryyr.net>
2026-07-30 23:39:50 +0900

    公開ファイルから配備先の名前を外す（公開範囲の規則）

--- tests/e2e/endurance-sdk.test.ts
+++ tests/e2e/endurance-sdk.test.ts
@@ -16,8 +16,8 @@
  * 実行:
  *
  * ```
- * export WHESO_PARTYKIT_PROJECT=cron-test
- * export WHESO_LIVE_HOST=cron-test.ryyr-ry.partykit.dev
+ * export WHESO_PARTYKIT_PROJECT=<配備先の企画>
+ * export WHESO_LIVE_HOST=<配備先のホスト>
  * WHESO_ENDURANCE_SEC=1800 npm run test:endurance:sdk    # 30 分
  * WHESO_ENDURANCE_SEC=180  npm run test:endurance:sdk    # 手早く確かめる
  * ```
